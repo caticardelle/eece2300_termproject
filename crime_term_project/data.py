205b377d95b3333801dc2ff167555a770084cae1
Catalina Cardelle <cardelle.c@husky.neu.edu>
2018-12-04 10:38:50 -0500

    commiting Regression and models

--- crime_term_project/data.py
+++ crime_term_project/data.py
@@ -1,10 +1,9 @@
-
 """
 Module for data handling. Specifically, Crime Dataset.
 """
 import pandas as pd
+from sklearn import preprocessing
 import numpy as np
-
 
 
 def load_data(filename):
@@ -16,8 +15,7 @@ def load_data(filename):
 
     df = pd.read_csv(filename, header=None)
 
-    return (df)
-
+    return df
 
 
 def summarize_data (df) :
@@ -41,36 +39,47 @@ def summarize_data (df) :
     return (df)
 
 
-
-def label_data (attributesfile, df):
+def label_data(attributesfile, df):
     """
     Function that loads the attributes file and adds it to dataframe as column labels
     :param attributesfile: file with attributes
     :param df: dataframe with data
     :return: labeled dataframe
     """
-    attributes = pd.read_csv(attributesfile, header=None)  #creates dataframe from attributes file
-    temp = pd.DataFrame()   #temporary dataframe to store attributes in
-    attribute = attributes[0].str.split(' ').str[1] #gets just the attribute name
+    attributes = pd.read_csv(attributesfile, header=None)  # creates dataframe from attributes file
+    temp = pd.DataFrame()  # temporary dataframe to store attributes in
+    attribute = attributes[0].str.split(' ').str[1]  # gets just the attribute name
     temp[0] = attribute
     temp = temp.transpose()
-    header = temp.iloc[0]   #gets the row of attributes from temp
-    labeled_df = df.rename(columns = header)    #sets the attributes as column labels
-    return(labeled_df)
+    header = temp.iloc[0]  # gets the row of attributes from temp
+    labeled_df = df.rename(columns=header)  # sets the attributes as column labels
+    return (labeled_df)
 
-def clean_data (df2):
+
+def encode_labels(unordered_labels):
+    """
+
+    :param label_set:
+    :return:
+    """
+
+    le = preprocessing.LabelEncoder()
+    le.fit(unordered_labels)
+    return le.transform(unordered_labels)
+
+
+def clean_data(df2):
     """
     Function to delete columns with 10% data missing
     :param df2: summarized dataframe
     :return: cleaned df
     """
-    num_rows = int(len(df2.index.values)-1) #gets number of attributes
+    num_rows = int(len(df2.index.values) - 1)  # gets number of attributes
     for column in df2:
-        if df2.loc['Total_Stat_Count'][column] < (.90*num_rows):    #gets rows with more than 10% data points missing
-            df2 = df2.drop([column],  axis=1)   #deletes rows
+        if df2.loc['Total_Stat_Count'][column] < (.90 * num_rows):  # gets rows with more than 10% data points missing
+            df2 = df2.drop([column], axis=1)  # deletes rows
     df2 = df2.drop(['Total_Stat_Count'], axis=0)
     return df2
-
 
 
 def select_attributes(list_attributes):
@@ -81,7 +90,8 @@ def select_attributes(list_attributes):
     """
     pass
 
-#potential fcn to delete rows with missing data points
+
+# potential fcn to delete rows with missing data points
 
 def decision_tree_model(df):
     """
@@ -91,6 +101,7 @@ def decision_tree_model(df):
     """
     pass
 
+
 def naive_bayesian_model(df):
     """
        Function to call model on our dataframe and classifies cities
@@ -99,7 +110,8 @@ def naive_bayesian_model(df):
        """
     pass
 
-def perf_eval (our_results, actual_results):
+
+def perf_eval(our_results, actual_results):
     """
     Function to compare our results to the original data
     :param our_results: results from model
@@ -110,15 +122,21 @@ def perf_eval (our_results, actual_results):
 
 
 def main():
-    df = load_data('C:\\Users\\catic\\Documents\\EECE 2300\\python\\crime_term_project\\data\\raw\\communities.data.txt')
-    #print (df)
+    df = load_data(
+        'C:\\Users\\catic\\Documents\\EECE 2300\\python\\crime_term_project\\data\\raw\\communities.data.txt')
+    # print (df)
     df2 = summarize_data(df)
     #print(df2)
-    df_attributes = label_data('C:\\Users\\catic\\Documents\\EECE 2300\\python\\crime_term_project\\data\\raw\\communities.attributes.txt', df2)
-    #print(df_attributes)
+    df_attributes = label_data(
+        'C:\\Users\\catic\\Documents\\EECE 2300\\python\\crime_term_project\\data\\raw\\communities.attributes.txt',
+        df2)
+    # print(df_attributes)
     cleaned_df = clean_data(df_attributes)
-    #print (cleaned_df)
+    old_labels = list(cleaned_df['state'].astype(np.int))
+    new_labels = encode_labels(old_labels)
+    print (cleaned_df)
+
 
 if __name__ == '__main__':
-    main()
 
+    main()
