591b1b839613afc03abd96df02676b6e15c4ff80
Catalina Cardelle <cardelle.c@husky.neu.edu>
2018-12-05 21:44:09 -0500

    commiting changes in regression and models

--- crime_term_project/Models.py
+++ crime_term_project/Models.py
@@ -1,1 +1,53 @@
-import Regression
+import regression
+import numpy as np
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.grid_search import GridSearchCV
+
+y = regression.y.to_frame()
+x = regression.x.ix[:, regression.lassocv(regression.x, y)]
+
+y['CrimeCategory'] = np.where(y['ViolentCrimesPerPop']<.25,'low','medium')
+y['CrimeCategory'] = np.where(y['ViolentCrimesPerPop']>.4,'high', y['CrimeCategory'])
+Y = y['CrimeCategory']
+
+
+def decisiontree(X,y):
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=47, test_size=0.25)
+    clf = DecisionTreeClassifier(criterion='entropy')
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print('Accuracy Score on train data: ', accuracy_score(y_true=y_train, y_pred=clf.predict(X_train)))
+    print('Accuracy Score on test data: ', accuracy_score(y_true=y_test, y_pred=y_pred))
+    clf = DecisionTreeClassifier(criterion='entropy', min_samples_split=50)
+    clf.fit(X_train, y_train)
+    print('Accuracy Score on train data: ', accuracy_score(y_true=y_train, y_pred=clf.predict(X_train)))
+    print('Accuracy Score on the test data: ', accuracy_score(y_true=y_test, y_pred=clf.predict(X_test)))
+    dtc = DecisionTreeClassifier()
+    cv_scores = cross_val_score(dtc, X, y, cv=10)
+    sns.distplot(cv_scores)
+    plt.title('Average score: {}'.format(np.mean(cv_scores)))
+    plt.show()
+
+    parameter_grid = {'criterion': ['gini', 'entropy'],
+                      'splitter': ['best', 'random'],
+                      'max_depth': [None, 1, 2, 3, 4, 5],
+                      'max_features': [None, 1, 2, 3, 4]}
+
+    grid_search = GridSearchCV(dtc, param_grid=parameter_grid, cv=10)
+
+    grid_search.fit(X, y)
+    print('Best score: {}'.format(grid_search.best_score_))
+    print('Best parameters: {}'.format(grid_search.best_params_))
+
+    dtc = grid_search.best_estimator_
+    print(dtc)
+
+
+decisiontree(x, Y)
---
+++ crime_term_project/regression.py
@@ -0,0 +1,151 @@
+import data
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LinearRegression, LassoCV, RidgeCV, ElasticNetCV
+from sklearn import metrics
+from sklearn import preprocessing
+
+df = data.load_data('C:\\Users\\catic\\Documents\\EECE 2300\\python\\crime_term_project\\data\\raw\\communities.data.txt')
+df2 = data.summarize_data(df)
+df_attributes = data.label_data('C:\\Users\\catic\\Documents\\EECE 2300\\python\\crime_term_project\\data\\raw\\communities.attributes.txt', df2)
+cleaned_df = data.clean_data(df_attributes)
+cleaned_df = cleaned_df.replace('?', np.NaN)
+cleaned_df = cleaned_df.dropna(axis=0)
+
+
+x = cleaned_df.drop(['communityname','ViolentCrimesPerPop'], axis = 1)
+y = cleaned_df['ViolentCrimesPerPop']
+x_labels = x.columns
+x_as_array = x.values
+min_max_scale = preprocessing.MinMaxScaler()
+x_scaled = min_max_scale.fit_transform(x_as_array)
+x = pd.DataFrame(x_scaled)
+x.columns = x_labels
+
+train_x, test_x, train_y, test_y = train_test_split(x,y, test_size=.3, random_state=1)
+
+def linreg(x,y):
+    """
+    FUnction for linear regression
+    :param x: Attributes
+    :param y: target variable
+    :return: MSE
+    """
+    lm = LinearRegression()
+    lm.fit(train_x, train_y)
+    predict_test_lm = lm.predict(test_x)
+    plt.figure(figsize=(15, 10))
+    mse = metrics.mean_squared_error(test_y, predict_test_lm)
+    ft_importances_lm = pd.Series(lm.coef_, index=x.columns)
+    ft_importances_lm.plot(kind='barh')
+    plt.title("Linear Regression Coefficients \n Mean Squared Error = %f" % mse, fontsize=18)
+    plt.xlim(-.6, .6)
+    plt.show()
+    print(mse)
+
+
+def lassocv(x, y):
+    """
+    Function for Lasso cross validation
+    :param x: Attributes
+    :param y: Target
+    :return: MSE and list of attributes with highest weight
+    """
+    reg = LassoCV(cv=10, random_state=1).fit(x, y)
+    predictions = reg.predict(x)
+    mse = metrics.mean_squared_error(y, predictions)
+    plt.figure(figsize=(15, 10))
+    coefs = pd.Series(reg.coef_, index=x.columns)
+    absolute_coefs = coefs.sort_values()
+    top_coefs = abs(absolute_coefs).sort_values(ascending=False).head(18)
+    absolute_coefs.plot(kind='barh')
+    plt.title("Lasso Coefficents \n Mean Squared Error = %f" % mse, fontsize=18)
+    plt.xlim(-.6, .6)
+    plt.show()
+    print(reg.alpha_)
+    print(mse)
+    return list(top_coefs.index)
+    #return absolute_coefs.sort_values(ascending=False)
+
+
+def ridgecv(x, y):
+    """
+    Function for Ridge with Cross Validation
+    :param x: Attributes
+    :param y: Target
+    :return: MSE
+    """
+    reg = RidgeCV(cv=10).fit(x, y)
+    predictions = reg.predict(x)
+    mse = metrics.mean_squared_error(y, predictions)
+    plt.figure(figsize=(15, 10))
+    ft_importances_lm = pd.Series(reg.coef_[0], index=x.columns).sort_values()
+    absolute_coefs = pd.Series(reg.coef_[0], index=x.columns)
+    print(absolute_coefs.sort_values(ascending=False))
+    ft_importances_lm.plot(kind='barh')
+    plt.title("Ridge Coefficents \n Mean Squared Error = %f" % mse, fontsize=18)
+    plt.xlim(-.6, .6)
+    plt.show()
+    print(reg.alpha_)
+    return mse
+
+
+def elasticnetcv(x, y):
+    """
+    FUnction for Elastic Net with Cross Validation
+    :param x: Attributes
+    :param y: Target
+    :return: MSE
+    """
+    reg = ElasticNetCV(cv=10, random_state=1).fit(x, y)
+    predictions = reg.predict(x)  # predicts the target variable
+    mse = metrics.mean_squared_error(y, predictions)  # gets MSE from the true values and the predicted values
+    plt.figure(figsize=(15, 10))
+    ft_importances_lm = pd.Series(reg.coef_, index=x.columns)
+    absolute_coefs = pd.Series(reg.coef_, index=x.columns)
+    print(absolute_coefs.sort_values(ascending=False))
+    ft_importances_lm.plot(kind='barh')
+    plt.title("Elastic Net Coefficents \n Mean Squared Error = %f" % mse, fontsize=18)
+    plt.xlim(-.6, .6)
+    plt.show()
+    print(reg.alpha_)
+    return mse
+
+
+def visualization(topcoefs):
+    """
+
+    :param topcoefs:
+    :return:
+    """
+    plt.subplot(221)
+    plt.scatter(cleaned_df.ix[:, topcoefs[0]], cleaned_df.ix[:, 'ViolentCrimesPerPop'])
+    plt.title(topcoefs[0])
+    plt.subplot(222)
+    plt.scatter(cleaned_df.ix[:, topcoefs[1]], cleaned_df.ix[:, 'ViolentCrimesPerPop'])
+    plt.title(topcoefs[1])
+    plt.subplot(223)
+    plt.scatter(cleaned_df.ix[:, topcoefs[2]], cleaned_df.ix[:, 'ViolentCrimesPerPop'])
+    plt.title(topcoefs[2])
+    plt.subplot(224)
+    plt.scatter(cleaned_df.ix[:, topcoefs[3]], cleaned_df.ix[:, 'ViolentCrimesPerPop'])
+    plt.title(topcoefs[3])
+    plt.show()
+
+
+
+fig, ax = plt.subplots()
+size = cleaned_df['racepctblack']*100
+cleaned_df.plot.scatter(x='PctIlleg', y='PctKids2Par', s=size, c='ViolentCrimesPerPop',
+                        colormap='Reds', ax=ax)
+plt.ylabel("Pct kids in family housing w/ two parents")
+plt.xlabel("Pct kids born to never married")
+
+
+#print(linreg(x,y))
+#print(lassocv(x,y))
+print(visualization(lassocv(x,y)))
+#print(ridgecv(x,y))
+#print(elasticnetcv(x,y))
